b88c3c7c16995def446590e740da760c72a7c77c
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-06-09 06:44:53 +0300

    commit tree dcb2a97b969960095173fa024b1e403ac5f27fe5

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNo){
+
+  if (blockNo > 42){
+      return blockNo - 42;
+  }
+  else{
+      return 42 - blockNo;
+  }
+}
+
+
+function distanceFromHqInFeet (blockNo) {
+// calls the distanceFromHqInBlocks function to return the calculation
+    return distanceFromHqInBlocks (blockNo) *264 ;
+
+
+}
+
+function distanceTravelledInFeet (start, destination){
+    if ((destination-start) > 0){
+        return (destination-start) *264;
+    }
+    else{
+        return ((destination-start) * -1) *264;
+    }
+
+}
+
+function calculatesFarePrice(start, destination) {
+    if ((destination-start) > 0){
+        let distance = (destination-start) * 264 ;
+        if ( (distance) < 401) {
+            return 0;
+        }
+        else if ((distance) < 2001) {
+            return ((distance)-400)* 0.02;
+        }
+        else if ((distance) < 2500) {
+            return 25 ;
+        }
+        else {
+            return 'cannot travel that far';
+        }
+    }
+    else{
+       let distance = ((destination-start) * -1)*264;
+        if ( (distance) < 401) {
+            return 0;
+        }
+        else if ((distance) < 2001) {
+            return ((distance)-400)* 0.02;
+        }
+        else if ((distance) < 2500) {
+            return  25 ;
+        }
+        else {
+            return 'cannot travel that far';
+        }
+    }
+    
+}
